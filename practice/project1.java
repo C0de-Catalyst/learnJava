01341761f97e572bdf77c28c1310a7e86e13c4d5
CodeCatalyst <codecatalyst99@gmail.com>
2026-06-06 00:42:18 +0530

    Add files via upload

---
+++ practice/project1.java
@@ -0,0 +1,20 @@
+public class project1 {
+    public static void main (String[] args){
+        
+    // Student Data 
+    String studentName = "Devi Prasad";
+    int studentAge = 18;
+    int studentRollNumber = 32 ;
+    int studentAdmissionNumber =6733 ;
+    float studentfee = 8730.35f ;
+    char studentGrade = 'B' ;
+
+    System.out.println("Student Name : " + studentName);
+    System.out.println("Student age : " + studentAge);
+    System.out.println("Student Roll Number : " + studentRollNumber);
+    System.out.println("Student Admission Number : " + studentAdmissionNumber);
+    System.out.println("Student fee : " + studentfee);
+    System.out.println("Student Grade : " + studentGrade);
+
+    }    
+}
---
+++ practice/project2.java
@@ -0,0 +1,15 @@
+public class project2 {
+    public static void main (String[] args) {
+        // a real life example of calculating the are of a Rectangle
+    int lenght = 52 ;
+    int breadth = 26 ;
+    int area = lenght * breadth ;
+    int perimeter = 2 * (lenght + breadth) ;
+    
+    System.out.println("lenght = " + lenght);
+    System.out.println("breadth = " + breadth);
+    System.out.println("Area of Rectangle = " + area);
+    System.out.println("Perimeter of Rectangle = " + perimeter);
+
+    }
+}
