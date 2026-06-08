a3de0ce136620c6ba3e39aed0859353dcb99802d
CodeCatalyst <codecatalyst99@gmail.com>
2026-06-08 21:40:27 +0530

    Add files via upload

---
+++ practice/RealLifeExample.java
@@ -0,0 +1,14 @@
+public class RealLifeExample {
+    public static void main(String[] args) {
+
+        int items = 29;
+        float price = 49.69f;
+        float total = items * price;
+        char currency = '$';
+
+
+        System.out.println("Number of items = " + items);
+        System.out.println("Price per item = " + currency + price);
+        System.out.println("Total = " + currency + total);
+        }
+}
