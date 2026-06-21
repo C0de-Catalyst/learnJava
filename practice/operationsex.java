043e2facd7223a4c6b4ecb8e06c75fb0bd6e06e2
CodeCatalyst <codecatalyst99@gmail.com>
2026-06-22 00:52:12 +0530

    Add files via upload

---
+++ practice/operationsex.java
@@ -0,0 +1,16 @@
+public class operationsex{
+    public static void main(String[] args){
+
+    int peopleinroom = 0;
+     
+    peopleinroom++;
+    peopleinroom++;
+    peopleinroom++;
+    peopleinroom++;
+
+    System.out.println("People in room: " + peopleinroom);
+    
+    peopleinroom--;
+    System.out.println("People in room: " + peopleinroom);
+    }
+}
